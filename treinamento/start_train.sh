cf0c26ec5d8f848c8feb5b697b48dd734764ef33
kilderehenrique <kilderehenriquedp@gmail.com>
2024-11-14 07:36:09 -0300

    Alterando script de treinamento

--- treinamento/start_train.sh
+++ treinamento/start_train.sh
@@ -1,1 +1,13 @@
-yolo train model=yolo11n.pt data=data.yaml epochs=50 imgsz=640
+modelo=yolov8s.pt
+sz_img_compress=640
+
+qtd_epocas=50
+max_sem_melhoria=5
+
+# considera apenas presença e nao classificacao
+#single_cls=True
+
+# usado para transfer learning
+#freeze=NUM
+
+yolo train model=$modelo data=data.yaml imgsz=$sz_img_compress epochs=$qtd_epocas patience=$max_sem_melhoria
