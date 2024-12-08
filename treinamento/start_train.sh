70fe0cede41e10cbc0a4f1a68e39a95d99f83a73
kilderehenrique <kilderehenriquedp@gmail.com>
2024-12-08 09:58:27 -0300

    sh para o terinamento final

--- treinamento/start_train.sh
+++ treinamento/start_train.sh
@@ -1,4 +1,7 @@
-modelo=yolov8s.pt
+modelo=yolo11s.pt
+#modelo=BH_robo.pt
+#modelo=runs/detect/train/weights/best.pt
+
 sz_img_compress=640
 
 qtd_epocas=50
@@ -10,4 +13,8 @@ max_sem_melhoria=5
 # usado para transfer learning
 #freeze=NUM
 
-yolo train model=$modelo data=data.yaml imgsz=$sz_img_compress epochs=$qtd_epocas patience=$max_sem_melhoria
+yolo train model=$modelo data=data.yaml imgsz=$sz_img_compress epochs=$qtd_epocas 
+#freeze=23
+#classes=1
+#patience=$max_sem_melhoria
+#yolo train resume model=runs/detect/train2/weights/last.pt
